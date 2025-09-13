f20000f1836ab651cbbbb5ef160bc9eef77b164b
ravimehta251 <Ravi7481081raj@gmail.com>
2025-09-13 10:37:43 +0530

    third commit2

--- src/Component/Contact/Contact.jsx
+++ src/Component/Contact/Contact.jsx
@@ -1,12 +1,10 @@
-
-import './contact.css'
-import theme_pattern from '../../assets/theme_pattern.svg'
-import mail_icon from '../../assets/mail_icon.svg'
-import location_icon from '../../assets/location_icon.svg'
-import call_icon from '../../assets/call_icon.svg'
+import "./Contact.css";
+import theme_pattern from "../../assets/theme_pattern.svg";
+import mail_icon from "../../assets/mail_icon.svg";
+import location_icon from "../../assets/location_icon.svg";
+import call_icon from "../../assets/call_icon.svg";
 const contact = () => {
-
-const onSubmit = async (event) => {
+  const onSubmit = async (event) => {
     event.preventDefault();
     const formData = new FormData(event.target);
 
@@ -19,9 +17,9 @@ const onSubmit = async (event) => {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
-        Accept: "application/json"
+        Accept: "application/json",
       },
-      body: json
+      body: json,
     }).then((res) => res.json());
 
     if (res.success) {
@@ -29,45 +27,61 @@ const onSubmit = async (event) => {
     }
   };
 
-
-
-return (
-    <div id="contact" className='contact'>
-            <div className="contact-title">
-                    <h1>Get in Touch</h1>
-                    <img src={theme_pattern} alt="" />
+  return (
+    <div id="contact" className="contact">
+      <div className="contact-title">
+        <h1>Get in Touch</h1>
+        <img src={theme_pattern} alt="" />
+      </div>
+      <div className="contact-section">
+        <div className="contact-left">
+          <h1>Let's Talk</h1>
+          <p>
+            I'm always open to discussing new projects, creative ideas, or
+            opportunities to be part of your vision. Feel free to reach out using
+            the contact form or via email!
+          </p>
+          <div className="contact-details">
+            <div className="contact-detail">
+              <img src={mail_icon} alt="" />
+              <p>ravi7481081traj@gmail.com</p>
             </div>
-            <div className="contact-section">
-                    <div className="contact-left">
-                            <h1>Let's Talk</h1>
-                            <p>
-                                I'm always open to discussing new projects, creative ideas, or opportunities to be part of your vision. Feel free to reach out using the contact form or via email!
-                            </p>
-                            <div className="contact-details">
-                                <div className='contact-detail'>
-                                    <img src={mail_icon} alt="" /><p>ravi7481081traj@gmail.com</p>
-                                </div>
-                                <div className='contact-detail'>
-                                    <img src={call_icon} alt="" /><p>+91XXXXXXXXXX</p>
-                                </div>
-                                <div className='contact-detail'>
-                                    <img src={location_icon} alt="" /><p>Bengaluru,India</p>
-                                </div>
-                            </div>
-                    </div>
-                    <form onSubmit={onSubmit} className="contact-right">
-                        <label htmlFor="">Your Name</label>
-                        <input type="text" placeholder='Enter your name' name='name'/>
-                        <label htmlFor="">Your Email</label>
-                        <input type="email" placeholder='Enter your email' name='email' />
-                        <label htmlFor=""> Write Your Message Here</label>
-                        <textarea name="message"  rows='8' placeholder='Enter Your Message'></textarea>
-                        <button type="submit" className='contact-submit'>Submit Now</button>
-
-                    </form>
+            <div className="contact-detail">
+              <img src={call_icon} alt="" />
+              <p>+91XXXXXXXXXX</p>
             </div>
+            <div className="contact-detail">
+              <img src={location_icon} alt="" />
+              <p>Bengaluru,India</p>
+            </div>
+          </div>
+        </div>
+        <form onSubmit={onSubmit} className="contact-right">
+          <label htmlFor="">Your Name</label>
+          <input
+            type="text"
+            placeholder="Enter your name"
+            name="name"
+          />
+          <label htmlFor="">Your Email</label>
+          <input
+            type="email"
+            placeholder="Enter your email"
+            name="email"
+          />
+          <label htmlFor=""> Write Your Message Here</label>
+          <textarea
+            name="message"
+            rows="8"
+            placeholder="Enter Your Message"
+          ></textarea>
+          <button type="submit" className="contact-submit">
+            Submit Now
+          </button>
+        </form>
+      </div>
     </div>
-)
-}
+  );
+};
 
-export default contact
+export default contact;
